35cfff81ed64b7a3edc23d6cd14d3b91563d4722
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-06-08 07:14:00 +0000

    [Add] Bootstrapの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  resources :books
+  root to: 'homes#top'
+  get 'homes/about', as: 'about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
