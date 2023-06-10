aeecb5f0e2538f7b2b09dceb652f99f184cde632
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-06-10 07:41:14 +0000

    [Add] アクセス制限

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,5 @@ class Book < ApplicationRecord
   has_one_attached :image
   
    validates :title, presence: true
-   validates :body, presence: true
+   validates :body, presence: true, length: { maximum: 200 }
 end
